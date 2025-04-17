457e3b703b98f10d755bb494ebc1c7d5459103a7
Lucijane <lucijanejusto@gmail.com>
2025-04-17 00:00:48 -0300

    feat:Add componentes

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import { AppBar } from "@mui/material"
+import Navbar from "../../Navbar/Navbar"
+import Hero from "./sections/Hero/Hero"
+
+const Home = () => {
+
+    return (
+        <>
+            <Navbar />
+            <Hero />
+        </>
+    )
+}
+
+export default Home
